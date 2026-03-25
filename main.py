a0122e1467e78f9b274f7cb66723d41c9d26abfc
martindmv <martin.demerdjiev@epfedu.fr>
2026-03-25 12:48:12 +0100

    Defining classes Portfolio, Experience, Project, Skill

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Skill(BaseModel):
+    id : int
+    name : str 
+    level : str
+
+class Project(BaseModel):
+    id : int
+    name : str 
+    description : str
+    link : str | None = None
+
+class Experience(BaseModel):
+    id : int
+    company : str 
+    role : str
+    duration : str
+    description : str
+
+class Portfolio(BaseModel):
+    id : int
+    name : str 
+    formation : str
+    experience : list[Experience]
+    projects : list[Project]
+    skills : list[Skill]
+    Github : str
+    Linkdin : str
+
+
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Title": "E-portfolio"}
 
 
 @app.get("/items/{item_id}")
@@ -13,4 +44,4 @@ def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
 
 
-
+@app.get("/portfolio/{portfolio_id}")
